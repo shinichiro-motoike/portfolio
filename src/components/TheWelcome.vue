634b1a6b4de14deeac29a96e814d9fce298e894a
Shinichiro Motoike <motoike.shi@gmail.com>
2025-03-24 19:01:12 +0900

    feat: プロフィール情報を掲載する

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,5 +1,40 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>hoge</div>
+  <div>
+    <dl>
+      <dt>GitHub</dt>
+      <dd>
+        <a href="https://github.com/shinichiro-motoike" target="_brank" rel="nofollow"
+          >shinichiro-motoike</a
+        >
+      </dd>
+    </dl>
+    <dl>
+      <dt>X (Twitter)</dt>
+      <dd><a href="https://x.com/moto_shin_" target="_brank" rel="nofollow">moto_shin_</a></dd>
+    </dl>
+    <dl>
+      <dt>Zenn</dt>
+      <dd><a href="https://zenn.dev/motoshin" target="_brank" rel="nofollow">motoshin</a></dd>
+    </dl>
+    <dl>
+      <dt>Qiita</dt>
+      <dd>
+        <a href="https://qiita.com/shin_moto" target="_brank" rel="nofollow">shin_moto</a>
+      </dd>
+    </dl>
+  </div>
 </template>
+
+<style scoped>
+dl {
+  font-size: 20px;
+}
+
+dt {
+  display: block;
+  float: left;
+  width: 150px;
+}
+</style>
