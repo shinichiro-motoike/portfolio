e2b1ea5780a2d1b41e1be5ee024cff960eec2de8
Shinichiro Motoike <motoike.shi@gmail.com>
2025-03-24 19:03:39 +0900

    fix: TheWelcome コンポーネントを ProfileLinks コンポーネントにリネームする

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ProfileLinks from '../components/ProfileLinks.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ProfileLinks />
   </main>
 </template>
