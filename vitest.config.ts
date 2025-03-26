d7a2d646b5a6bffe7622af61e1c946a0498c8e25
Shinichiro Motoike <motoike.shi@gmail.com>
2025-03-26 18:49:56 +0900

    feat: カバレッジに含めないパスを指定する

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,17 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       coverage: {
+        exclude: [
+          'env.d.ts',
+          'eslint.config.ts',
+          'playwright.config.ts',
+          'vite.config.ts',
+          'vitest.config.ts',
+          //TODO: 以下、暫定的に除外している
+          'src/App.vue',
+          'src/main.ts',
+          'src/components/icons/**',
+        ],
         provider: 'v8',
         reporter: ['text', 'json', 'html'],
         reportsDirectory: './tests/unit/coverage',
